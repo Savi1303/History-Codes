142ec1abec24a36c1fda45243629b424e2806113
Smart Saviour <savioursmart213@gmail.com>
2023-02-05 01:43:10 +0100

    Add files via upload

---
+++ domsut/savi.js
@@ -0,0 +1,112 @@
+function changeImg() {
+    let top = document.querySelector(".sav");
+    let middle = document.querySelector(".h2");
+    let eMiddle = document.querySelector(".head");
+    let bottom = document.querySelector(".para");
+
+    top.src ="./images/download (1).jpeg";
+    middle.innerHTML = "Nike Sneakers";
+    eMiddle.innerHTML = "Color : Blue only";
+    bottom.innerHTML = "$20";
+}
+
+function changeImg2() {
+    let top = document.querySelector(".sav");
+    let middle = document.querySelector(".h2");
+    let eMiddle = document.querySelector(".head");
+    let bottom = document.querySelector(".para");
+
+    top.src ="./images/download.jpeg";
+    middle.innerHTML = "Jordan Special-type Sneakers";
+    eMiddle.innerHTML = "Unisex-type";
+    bottom.innerHTML = "$60";
+
+}
+
+function changeImg3() {
+    let top = document.querySelector(".sav");
+    let middle = document.querySelector(".h2");
+    let eMiddle = document.querySelector(".head");
+    let bottom = document.querySelector(".para");
+
+    top.src ="./images/2685_1610988379.jpeg";
+    middle.innerHTML = "Air Jordan One-Royal Sneakers";
+    eMiddle.innerHTML = "Unisex-type";
+    bottom.innerHTML = "$43";
+}
+
+function changeImg4() {
+    let top = document.querySelector(".sav");
+    let middle = document.querySelector(".h2");
+    let eMiddle = document.querySelector(".head");
+    let bottom = document.querySelector(".para");
+
+    top.src ="./images/OIP.jpeg";
+    middle.innerHTML = "Special Sports Shoes";
+    eMiddle.innerHTML = "Men Sports Shoes";
+    bottom.innerHTML = "$36";
+} 
+
+// // (a) => { 
+// //     return  + 100; }
+
+// let myHonda = {
+//     color: "red", 
+//     wheels: 4,
+//     engine: {
+//         cylinders: 4,
+//         size: 2.2
+//     },
+// }
+//  console.log (myHonda);
+
+                                //  costructors
+
+// function Car(make, model, year) {
+//     this.make = Japan;
+//     this.model = Taskishima;
+//     this.year = 2013;
+// } console.log(Car)
+
+// const myCar = new Car("Germany","VolksWagon",2020);
+// console.log(myCar)
+
+// function Person(name,age,sex) {
+//     this.name = name;
+//     this.age = age;
+//     this.sex = sex;
+// } const jake = new Person("savi",13,"male");
+//   const david = new Person("jerry",13,"male");
+
+// function Car(make, model, year, owner) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//     this.owner = owner;
+// } 
+// const myCar = new Car("omnix", "JS2015", 2020, "john");
+// console.log(myCar)
+
+// let car1 = new Car("Falcon", "Soar Depth", 2023, "jake");
+//  car1.color = "black";
+// console.log(car1);
+
+// const car2 = new Car("Toyota", "HighLander", 2019, "david");
+// console.log(car2)
+
+
+// const Animal = {
+//     type : "invertebrates",
+//     diplayType() {
+//         console.log(this.type);
+//     },
+// };
+
+// const animal1 = Object.create(Animal);
+// animal1.displayType()
+// console.log(invertebrates)
+
+// const fish = Object.create(Animal);
+// fish.type = "Fishes";
+// fish.diplayType();
+// console.log(Fishes)
